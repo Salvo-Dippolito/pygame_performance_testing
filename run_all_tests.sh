ecef382ff81b6c3b364e6275d7cba3b59e311165
Salvo-Dippolito <salvo.dippolito@gmail.com>
2024-02-05 05:43:37 +0100

    fixed bash runner

--- run_all_tests.sh
+++ run_all_tests.sh
@@ -9,7 +9,7 @@ log_file_name="Trials_$(date +'%Y_%m_%d_%H%M%S').txt"
 # Save the log file name to the temporary file
 echo "$log_file_name" > "$temp_file"
 
-for i in {0..45}
+for i in {0..1}
 do
     # Run the Python script to update silver token coordinates
     python3 update_coordinates.py
@@ -18,16 +18,17 @@ do
     echo -e "--------------------------- \n test_${i} assignment_1_controller" >> "./logs/$log_file_name"
     python3 TEST_run.py assignment_1_controller.py &
     sleep 200
-    kill -9 $!
+    pkill -9 -f assignment_1_controller.py
 
     # Run benchmark_controller.py with the log file as an argument
     echo -e "--------------------------- \n test_${i} benchmark_controller" >> "./logs/$log_file_name"
     python3 TEST_run.py benchmark_controller.py &
     sleep 200
-    kill -9 $!
+    pkill -9 -f benchmark_controller.py
 done
 
 echo -e "--------------------------- \n" >> "./logs/$log_file_name"
 
 # Remove the temporary file after use
 rm "$temp_file"
+
